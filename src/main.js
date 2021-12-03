42a7b2b3e4fdbb828bd82e8f406f7a4f83d78a49
yuuchin <shontzuforever@gmail.com>
2021-12-03 13:17:45 +0800

    vue-material card

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 // you can import anything here: bootstrap, css, libraries...
 // import BootstrapCarousels from ''
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
 
 new Vue({
   router,
