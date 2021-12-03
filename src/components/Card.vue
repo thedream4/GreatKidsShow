47ac2bf9f06ea104e8c787a091d2da9dae1a8dfe
yuuchin <shontzuforever@gmail.com>
2021-12-03 12:41:23 +0800

    created card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Card',
   props: {
     msg: String
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card imsg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
