5e5a94c3d0f9ef27f91197c32ccedf2ef4fc44f0
yuuchin <shontzuforever@gmail.com>
2021-12-23 23:06:42 +0800

    welcome message

--- src/components/ModalWelcome.vue
+++ src/components/ModalWelcome.vue
@@ -3,12 +3,12 @@
     <b-button v-b-modal.modal-multi-1 ref="myBtn" style="visibility: hidden"></b-button>
 
     <b-modal  id="modal-multi-1"  size="lg" title="WELCOME"  ok-only no-stacking >
-      <p class="my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt magnam vel ratione omnis modi? Sed exercitationem quos dicta nam sit?</p>
-      <b-button v-b-modal.modal-multi-2>Next</b-button>
+      <h1 class="my-2">Welcome to the Great Kids Show!</h1>
+      <b-button v-b-modal.modal-multi-2>What is the Great Kids Show?</b-button>
     </b-modal>
 
-    <b-modal id="modal-multi-2" title="WELCOME" ok-only>
-      <p class="my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nihil quis exercitationem, dicta vitae nobis blanditiis error accusamus similique officiis.</p>
+    <b-modal id="modal-multi-2" title="THE GREAT KIDS SHOW" ok-only>
+      <p class="my-2">The Great Kids Show is all about breeding entertainment with education. Here, we break complex information into bite-sized chunks that is convenient for everyday access. Our website is intended to look like an iPad game and mimic Fisher Price toys, because our goal is not to create something for kids to use, rather, to create something that kids are already using, and use that to provide value</p>
     </b-modal>
   </div>
 </template>
