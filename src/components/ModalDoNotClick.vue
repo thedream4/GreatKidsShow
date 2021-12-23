a877c72da101b5d46048bff2890243d9ef738f9c
yuuchin <shontzuforever@gmail.com>
2021-12-23 17:03:58 +0800

    sound effect and modals
    	1) how to randomly generate sound effect..? currently is hardcoded
    	2) how to toggle the modal visibility...

---
+++ src/components/ModalDoNotClick.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span>
+      <h1>I TOLD YOU NOT TO CLICK</h1>
+      <h3>*insert some funny gif here*</h3>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  //   on app mount, emit click event to trigger modal popup
+};
+</script>
+
+<style scoped>
+div {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color:white;
+  z-index:600;
+}
+</style>
---
+++ src/components/ModalWelcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-button v-b-modal.modal-multi-1 ref="myBtn" style="visibility: hidden"></b-button>
+
+    <b-modal  id="modal-multi-1"  size="lg" title="WELCOME"  ok-only no-stacking >
+      <p class="my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt magnam vel ratione omnis modi? Sed exercitationem quos dicta nam sit?</p>
+      <b-button v-b-modal.modal-multi-2>Next</b-button>
+    </b-modal>
+
+    <b-modal id="modal-multi-2" title="WELCOME" ok-only>
+      <p class="my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nihil quis exercitationem, dicta vitae nobis blanditiis error accusamus similique officiis.</p>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+//   on app mount, emit click event to trigger modal popup
+  mounted() {
+    const elem = this.$refs.myBtn;
+    elem.click();
+  },
+};
+</script>
+
+<style>
+</style>
